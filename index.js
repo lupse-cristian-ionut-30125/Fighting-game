96d916622cd5ee05ac11275107dd3523023f59c1
lupse-cristian-ionut-30125 <cristi@freshclick.ro>
2022-12-29 14:16:01 +0200

    Movement Characters

---
+++ index.js
@@ -0,0 +1,164 @@
+const canvas = document.querySelector("canvas");
+const canvasContext = canvas.getContext("2d");
+
+canvas.width = 1900;
+canvas.height = 900;
+
+canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.7;
+
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 300;
+    this.lastKey;
+  }
+
+  draw() {
+    canvasContext.fillStyle = "blue";
+    canvasContext.fillRect(this.position.x, this.position.y, 100, this.height);
+  }
+
+  update() {
+    this.draw();
+
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
+  }
+}
+
+const player1 = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+const player2 = new Sprite({
+  position: {
+    x: 800,
+    y: 200,
+  },
+  velocity: {
+    x: 0,
+    y: 10,
+  },
+});
+
+player2.draw();
+
+console.log(player1);
+
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+};
+
+let lastKey;
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  player1.update();
+  player2.update();
+
+  player1.velocity.x = 0;
+  player2.velocity.x = 0;
+
+  //player 1 movement
+  if (keys.a.pressed && player1.lastKey === "a") {
+    player1.velocity.x = -5;
+  } else {
+    if (keys.d.pressed && player1.lastKey === "d") {
+      player1.velocity.x = 5;
+    }
+  }
+
+  //player2 movement
+  if (keys.ArrowLeft.pressed && player2.lastKey === "ArrowLeft") {
+    player2.velocity.x = -5;
+  } else {
+    if (keys.ArrowRight.pressed && player2.lastKey === "ArrowRight") {
+      player2.velocity.x = 5;
+    }
+  }
+}
+
+animate();
+
+window.addEventListener("keydown", (event) => {
+  console.log(event.key);
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      player1.lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      player1.lastKey = "a";
+      break;
+    case "w":
+      player1.velocity.y = -20;
+      break;
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      player2.lastKey = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      player2.lastKey = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      player2.velocity.y = -2ff0;
+      break;
+  }
+  console.log(event.key);
+});
+window.addEventListener("keyup", (event) => {
+  //player1 keys
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+
+  //player2 keys
+  switch (event.key) {
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
+  }
+  console.log(event.key);
+});
